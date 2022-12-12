fac19bcd027c4796651f543e0102928822ba19df
Sapitorico <renzoyaques@gmail.com>
2022-12-12 12:25:21 -0300

    fix betty style in function  Run_command

--- Interprete_command.c
+++ Interprete_command.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
  * Run_Command - This function runs the command
- *
  * @command: Command
+ * @input: Input line
+ * @argv: Name of the executable
+ * @count_error: Error counter
+ * Return: Execution status
  */
-int Run_Command(command_t *command, char *input, char **argv, int counter_error)
+int Run_Command(command_t *command, char *input, char **argv, int count_error)
 {
 	command_t *head = NULL;
 	int status = 0;
@@ -16,39 +19,23 @@ int Run_Command(command_t *command, char *input, char **argv, int counter_error)
 	counter = Elements_Counter(command);
 	args = _calloc(counter + 1, sizeof(char *));
 	if (!args)
-	{
 		return (0);
-	}
 	while (command)
-	{
-		args[index] = command->args;
-		command = command->next;
-		index++;
-	}
+		args[index] = command->args, command = command->next, index++;
 	child_pid = fork();
 	if (child_pid == -1)
-	{
-		perror(argv[0]);
-		return (1);
-	}
+		perror(argv[0]), exit(1);
 	else if (child_pid == 0)
 	{
 		status = execve(args[0], args, environ);
 		if (status == -1)
-		{
 			fprintf(stderr, "%s: %d: %s: Permission denied\n",
-					argv[0], counter_error, args[0]);
-		}
-		free(input);
-		Free_Grid(args);
-		Free_List(head);
+					argv[0], count_error, args[0]);
+		free(input), Free_Grid(args), Free_List(head);
 		exit(EXIT_FAILURE);
 	}
 	else
-	{
 		waitpid(child_pid, &status, WUNTRACED);
-	}
-	Free_List(head);
-	Free_Grid(args);
+	Free_List(head), Free_Grid(args);
 	return (status);
 }
